ca0cd9227ccc58d1ba1734060bf88c7ffcd0f341
Evelna Brestavizky <berstovi@post.bgu.ac.il>
2020-03-28 17:20:47 +0300

    set to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ formButton.addEventListener('submit', (e) => {
     msg2.textContent = ''
 
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msg1.textContent = data.error
